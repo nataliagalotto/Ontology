d537ec1e500c8951128bfd215b8728eebfe25f83
nataliagalotto <nataliagalotto@gmail.com>
2019-01-31 13:52:01 -0200

    Projeto Criado

---
+++ src/main/java/com/projeto/generico/GenericoApplication.java
@@ -0,0 +1,14 @@
+package com.projeto.generico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericoApplication.class, args);
+	}
+
+}
+
