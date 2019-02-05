c733a224dbe158f820bd52ac08fe5386f58da011
nataliagalotto <nataliagalotto@gmail.com>
2019-02-05 19:36:23 -0200

    Criando Cidade e Estado

--- src/main/java/com/projeto/generico/GenericoApplication.java
+++ src/main/java/com/projeto/generico/GenericoApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.projeto.generico.domain.Categoria;
+import com.projeto.generico.domain.Cidade;
+import com.projeto.generico.domain.Estado;
 import com.projeto.generico.domain.Produto;
 import com.projeto.generico.repositories.CategoriaRepository;
+import com.projeto.generico.repositories.CidadeRepository;
+import com.projeto.generico.repositories.EstadoRepository;
 import com.projeto.generico.repositories.ProdutoRepository;
 
 //CommandLineRunner para permitir executar uma ação quando a aplicação iniciar
@@ -21,6 +25,11 @@ public class GenericoApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GenericoApplication.class, args);
@@ -46,6 +55,20 @@ public class GenericoApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+				
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2, c3));
+		
 	}
 
 }
