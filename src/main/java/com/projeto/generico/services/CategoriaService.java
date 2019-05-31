798768907fd09e4b4a752b12843e6bed4522acdc
nalmeida <nalmeida@uolinc.com>
2019-05-31 13:46:11 -0300

    Incluindo metodo que pega uma categoriaDTO e retorna uma Categoria

--- src/main/java/com/projeto/generico/services/CategoriaService.java
+++ src/main/java/com/projeto/generico/services/CategoriaService.java
@@ -3,6 +3,7 @@ package com.projeto.generico.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.projeto.generico.dto.CategoriaDTO;
 import com.projeto.generico.services.exception.DataIntegrityException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -57,6 +58,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO categoriaDTO){
+		return new Categoria(categoriaDTO.getId(), categoriaDTO.getNome());
 	}
 
 }
