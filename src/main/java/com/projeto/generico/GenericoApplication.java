76890a2d751c0eff626ea66f80a4007fe86e714a
nataliagalotto <nalmeida@uolinc.com>
2020-07-22 22:38:45 -0300

    Adicionando a conexão com o mysql

--- src/main/java/com/projeto/generico/GenericoApplication.java
+++ src/main/java/com/projeto/generico/GenericoApplication.java
@@ -36,113 +36,11 @@ import com.projeto.generico.repositories.ProdutoRepository;
 @SpringBootApplication
 public class GenericoApplication implements CommandLineRunner {
 
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private ProdutoRepository produtoRepository;
-
-	@Autowired
-	private EstadoRepository estadoRepository;
-	@Autowired
-	private CidadeRepository cidadeRepository;
-
-	@Autowired
-	private ClienteRepository clienteRepository;
-	@Autowired
-	private EnderecoRepository enderecoRepository;
-
-	@Autowired
-	private PedidoRepository pedidoRepository;
-	@Autowired
-	private PagamentoRepository pagamentoRepository;
-
-	@Autowired
-	private ItemPedidoRepository itemPedidoRepository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(GenericoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
-		Categoria cat3 = new Categoria(null, "Casa mesa e banho");
-		Categoria cat4 = new Categoria(null, "Eletrônicos");
-		Categoria cat5 = new Categoria(null, "Jardinagem");
-		Categoria cat6 = new Categoria(null, "Decoração");
-		Categoria cat7 = new Categoria(null, "Perfumaria");
-
-		Produto p1 = new Produto(null, "Computador", 2000.00);
-		Produto p2 = new Produto(null, "Impressora", 800.00);
-		Produto p3 = new Produto(null, "Mouse", 80.00);
-
-		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2));
-
-		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
-
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-
-		Estado est1 = new Estado(null, "Minas Gerais");
-		Estado est2 = new Estado(null, "São Paulo");
-
-		Cidade c1 = new Cidade(null, "Uberlândia", est1);
-		Cidade c2 = new Cidade(null, "São Paulo", est2);
-		Cidade c3 = new Cidade(null, "Campinas", est2);
-
-		est1.getCidades().addAll(Arrays.asList(c1));
-		est2.getCidades().addAll(Arrays.asList(c2, c3));
-
-		estadoRepository.saveAll(Arrays.asList(est1, est2));
-		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
-		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
-
-		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apt 40 ", "Jardim", "38111234", cli1, c1);
-		Endereco e2 = new Endereco(null, "Av Flores", "105", "Apt 40 ", "Centro", "38111234", cli1, c2);
-
-		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
-
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy HH:mm");
-
-		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
-		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:32"), cli1, e2);
-
-		Pagamento pag1 = new PagamentoComCartao(null, EstadoPagamento.PAGO, ped1, 6);
-		ped1.setPagamento(pag1);
-
-		Pagamento pag2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
-				null);
-		ped2.setPagamento(pag2);
-
-		cli1.getPedido().addAll(Arrays.asList(ped1, ped2));
-
-		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
-		pagamentoRepository.saveAll(Arrays.asList(pag1, pag2));
-
-		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
-		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
-		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
-
-		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
-		ped2.getItens().addAll(Arrays.asList(ip3));
-
-		p1.getItens().addAll(Arrays.asList(ip1));
-		p2.getItens().addAll(Arrays.asList(ip3));
-		p3.getItens().addAll(Arrays.asList(ip2));
-
-		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
-
 	}
-
 }
